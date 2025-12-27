d30625d474d0e161689a5b738c58a65d1453215c
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2025-12-27 18:44:21 +0100

    feat: deleted working directories and renamed

--- operator/tests/e2e/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-End-to-end tests for the Agentic Kubernetes Operator.
-
-These tests deploy actual resources to a Kubernetes cluster and validate
-that the operator creates the necessary deployments and that the system
-works end-to-end.
-"""
